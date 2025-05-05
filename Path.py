38e56ce4e89c336cab0e61fdfe6e8affcc98c228
Emory Temple-Asheim <78051348+emored@users.noreply.github.com>
2025-05-05 11:05:37 -0700

    Create Path.py

---
+++ Path.py
@@ -0,0 +1,29 @@
+import board # type: ignore
+import neopixel # type: ignore
+import time # type: ignore
+ 
+pixel_pin = board.D2
+num_pixels = 8        #number of leds pixels on the ring
+ 
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.7, auto_write=False)
+ 
+RED = (255, 0, 0) # RGB
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+OFF = (0, 0, 0)
+CUSTYL = (255, 150, 20)
+CUSTRD = (255, 30, 30)
+WHITE = (255, 255, 100)
+
+myCOLORS = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, CUSTYL, CUSTRD, WHITE]
+ 
+while True:
+    for i in range(num_pixels):
+        pixels[i] = RED
+        pixels.show()
+        time.sleep(0.5)
+        pixels[i] = OFF
+        pixels.show()
